ed7c55275ed9fac243e43761a04c077319154f2d
Marc Plogas <marc.plogas@digitalnatives.de>
2018-12-19 15:44:56 +0100

    startup script now uses madsonic.sh instead of binary

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-/usr/bin/madsonic --home=/config --default-music-folder=/media
+#/usr/bin/madsonic --home=/config --default-music-folder=/media
+/usr/share/madsonic/madsonic.sh --home=/config --context-path=/madsonic --default-music-folder=/media/music
 
 tail -F /config/madsonic_sh.log
